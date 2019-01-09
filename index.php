9c6eecf2c8d145dcff167c1486d4a4adde2e1091
BrightonVR <bvanrouendal@roc-dev.com>
2019-01-09 10:56:17 +0100

    test push

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brigh
+ * Date: 09/01/2019
+ * Time: 10:38
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<a href="Hoofdstuk01/index.php">Hoofdstuk 1 Index</a> <br>
+<a href="Hoofdstuk02/index.php">Hoofdstuk 2 Index</a>
+
+</body>
+</html>
